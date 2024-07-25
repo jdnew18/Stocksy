241f1b31ee01420aee317681916315c207357700
Jason <jdnew18@gmail.com>
2024-07-25 13:21:58 -0400

    Update to Stock database creation script to populate Quote table.

--- Stock/Database/CreateStockDatabase.sh
+++ Stock/Database/CreateStockDatabase.sh
@@ -1,2 +1,3 @@
 sqlite3 StockDatabase.db < CreateStockDatabase.sql
 sqlite3 StockDatabase.db '.mode tabs' '.import StockList.tsv Stock' 'select * from Stock;'
+sqlite3 StockDatabase.db '.mode tabs' '.import QuoteList.tsv Quote' 'select * from Quote;'
